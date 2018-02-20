3caf6416a0f756b7af90b0ad7e9d5cd4fda040f8
Dayna Blackwell <daynajblackwell@gmail.com>
2018-02-20 06:53:04 -0700

    Fixed typo.

--- pt 2/webpack.config.js
+++ pt 2/webpack.config.js
@@ -1,6 +1,15 @@
 // This library allows us to combine paths easily
+
+// modules are to node as libraries are to javascript
+// require('module) includes the module for use.
 const path = require('path');
 
+
+// the module object has a property called exports.  Exports
+// defines what the module makes available for other modules to use.
+
+// In other words: values, functions or objects can  be attached to modules.exports as properties
+// and these properties can be imported into other modules.  
 module.exports = {
    devServer: { contentBase: './src', publicPath: '/output' },
    entry: path.resolve(__dirname, 'src', 'index.jsx'),
